8c157ea2e8009995e29965b1a4c0d875db2acac0
dishowrov <dinislamshowrov.2001@gmail.com>
2023-12-13 16:23:49 +0600

    done

--- custom.js
+++ custom.js
@@ -3,115 +3,6 @@
 //     document.getElementById("vibe_bp_login").style.display = 'none';
 // }); 
 
-/*
-document.addEventListener("DOMContentLoaded", function () {
-    // Get references to the containers and buttons
-    const signinPart = document.querySelector('.dis-popup-signin-wrapper');
-    const signupPart = document.querySelector('.dis-popup-signup-wrapper');
-    const toggleButtons = document.querySelectorAll('.dis-toggle-btn');
-
-    // Function to show Sign In and hide Sign Up
-    function showSigninPart() {
-        signinPart.style.display = 'block';
-        signupPart.style.display = 'none';
-    }
-
-    // Function to show Sign Up and hide Sign In
-    function showSignupPart() {
-        signinPart.style.display = 'none';
-        signupPart.style.display = 'block';
-    }
-
-    // Add click event listeners to toggle buttons
-    toggleButtons.forEach(button => {
-        button.addEventListener('click', function () {
-            if (this.closest('.dis-popup-signin-part')) {
-                showSignupPart();
-            } else {
-                showSigninPart();
-            }
-        });
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    const signinWrapper = document.querySelector('.dis-popup-signin-wrapper');
-    const signupWrapper = document.querySelector('.dis-popup-signup-wrapper');
-    const popupLogin = document.querySelector('.popup_login');
-    const signinBtn = document.querySelector('.dis-popup-signin-btn');
-    const signupBtn = document.querySelector('.dis-popup-signup-btn');
-    const closeBtns = document.querySelectorAll('.dis-popup-close-btn');
-
-    function openSigninPopup() {
-        signinWrapper.style.display = 'block';
-        signupWrapper.style.display = 'none';
-        popupLogin.style.display = 'block';
-    }
-
-    function openSignupPopup() {
-        signinWrapper.style.display = 'none';
-        signupWrapper.style.display = 'block';
-        popupLogin.style.display = 'block';
-    }
-
-    function closePopup() {
-        popupLogin.style.display = 'none';
-    }
-
-    // Hide popupLogin initially
-    popupLogin.style.display = 'none';
-
-    // Add click event listeners
-    signinBtn.addEventListener('click', openSigninPopup);
-    signupBtn.addEventListener('click', openSignupPopup);
-
-    // Add click event listeners to all close buttons
-    closeBtns.forEach(btn => {
-        btn.addEventListener('click', closePopup);
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    // Initial setup: hide the popup and signup form
-    const popup = document.querySelector(".dis-mobile-popup");
-    popup.style.display = "none";
-    document.querySelector(".dis-mobile-popup-signup-form").style.display = "none";
-
-    // Function to toggle between signin and signup forms
-    function toggleForms(signinVisible) {
-        const signinForm = document.querySelector(".dis-mobile-popup-signin-form");
-        const signupForm = document.querySelector(".dis-mobile-popup-signup-form");
-
-        if (signinVisible) {
-            signinForm.style.display = "block";
-            signupForm.style.display = "none";
-        } else {
-            signinForm.style.display = "none";
-            signupForm.style.display = "block";
-        }
-    }
-
-    // Event listener for the main popup button
-    document.querySelector(".dis-mobile-popup-btn").addEventListener("click", function () {
-        popup.style.display = "block";
-    });
-
-    // Event listener for the signup button
-    document.querySelector(".dis-mobile-popup-signup-btn").addEventListener("click", function () {
-        toggleForms(false);
-    });
-
-    // Event listener for the signin button
-    document.querySelector(".dis-mobile-popup-signin-btn").addEventListener("click", function () {
-        toggleForms(true);
-    });
-
-    // Event listener for the close button
-    document.querySelector(".dis-mobile-popup-close-btn").addEventListener("click", function () {
-        popup.style.display = "none";
-    });
-});
-*/
 
 document.addEventListener("DOMContentLoaded", function () {
     // Part 1: Toggle between Sign In and Sign Up
@@ -201,7 +92,7 @@ document.addEventListener("DOMContentLoaded", function () {
         toggleForms(true);
     }
 
-    document.querySelector(".dis-mobile-popup-btn").addEventListener("click", openMobilePopup);
+    document.querySelector(".dis-mobile-open-popup-btn").addEventListener("click", openMobilePopup);
     document.querySelector(".dis-mobile-popup-signup-btn").addEventListener("click", function () {
         toggleForms(false);
     });
